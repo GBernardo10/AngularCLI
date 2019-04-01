021f414f1c00f742eed0996a121d63efa3342836
Guilherme Bernardo Muniz De Jesus <gbjesus1@stefanini.com>
2019-04-01 14:15:30 -0300

    bancoCOnexao

--- my-app/create-table.js
+++ my-app/create-table.js
@@ -1,40 +1,21 @@
-var sql = require("mssql");
-var app = express();
 var express = require('express');
 
+var sql = require("mssql");
+
+var app = express();
+
 app.get('/', function (req, res) {
-   
-    var sql = require("mssql");
 
     // config for your database
-	var config = {
-    user: 'adminGO',
-    password: 'GameOver0',
-    server: 'gameoverserve.database.windows.net',
-	database: 'DataBaseGO',
-    options: { encrypt: 'true', }
-};
+    var config = {
+        user: 'adminGO',
+        password: 'GameOver0',
+        server: 'gameoverserve.database.windows.net',
+        database: 'DataBaseGO',
+        options: { encrypt: 'true', }
+    };
 
-    // connect to your database
-    sql.connect(config, function (err) {
-    
-        if (err) console.log(err);
-
-        // create Request object
-        var request = new sql.Request();
-           
-        // query to the database and get the records
-        request.query('select * from users', function (err, recordset) {
-            
-            if (err) console.log(err)
-
-            // send records as a response
-            res.send(recordset);
-            
-        });
-    });
-});
-
-var server = app.listen(5000, function () {
-    console.log('Server is running..');
+    sql.connect(config)
+        .then(conn => console.log("conectou!"))
+        .catch(err => console.log("erro! " + err));
 });
